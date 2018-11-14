55499154e3ff9e82a7d7de69e3a1a9c8410c59f0
Loay <lhamayel2016@gmail.com>
2018-11-14 19:49:34 +0200

    SJF

--- src/projectoss1/FXMLDocumentController.java
+++ src/projectoss1/FXMLDocumentController.java
@@ -25,7 +25,7 @@ import javax.swing.JOptionPane;
 
 
 public class FXMLDocumentController implements Initializable {
-    int loaySharmoot1 ;
+    
       ArrayList<Process> processes;
     static ArrayList<Process> readyQueue;
     @FXML
@@ -79,7 +79,82 @@ public class FXMLDocumentController implements Initializable {
         }
         
         return true;
-    }   
+    }
+     
+      public ArrayList<Integer> SJF(){
+        int time = 0;                               // current time
+        Process currentlyRunning = null;            // the process that is running currently, initially null
+        readyQueue = new ArrayList<Process>();      // ready queue to add arrived processes that are ready to run
+        ArrayList<Integer> ganttChart = new ArrayList<Integer>();   // Gantt Chart to show processes run-time
+        
+        while(!allProcessesFinished()){     // while there are still unfinished processes
+            
+            checkProcessesArrival(time);    // check if any process has arriced at current moment
+            
+            if(currentlyRunning == null){               // if no process is running currently 
+                Process leastBurstTimeProcess = findLeastBurstTimeProcess();            // find the process with minimum burt time
+                int leastBursTimetProcessIdx = readyQueue.indexOf(leastBurstTimeProcess);   // find its index in the readyQueue
+                if (leastBursTimetProcessIdx != -1)                                     // and if it is in the queue
+                    currentlyRunning = readyQueue.remove(leastBursTimetProcessIdx);     // remove it and run it
+                
+                if(currentlyRunning != null && currentlyRunning.startTime == -1)    // if there is a process has just started to run
+                    currentlyRunning.startTime = time;                              // set its start time to current time
+            }
+            
+            if(currentlyRunning != null && time == currentlyRunning.burstTime + currentlyRunning.startTime){    // if current time = burst time + start time for a currently running process,
+                                                                                                                // then this means that the process has finished
+                currentlyRunning.finishTime = time;         // set finish time to current time
+                currentlyRunning.turnaround = currentlyRunning.finishTime - currentlyRunning.arrivalTime;   // compute turnaround time
+                currentlyRunning.waitingTime = currentlyRunning.turnaround - currentlyRunning.burstTime;    // compute waiting time
+                currentlyRunning = null;
+                
+                /* starting another process, just at the same time the previous process has finished */
+                Process leastBurstTimeProcess = findLeastBurstTimeProcess();            // find the process with minimum burst time
+                int leastBursTimetProcessIdx = readyQueue.indexOf(leastBurstTimeProcess);   // find its index in the readyQueue
+                if (leastBursTimetProcessIdx != -1)                                     // and if it is in the queue
+                    currentlyRunning = readyQueue.remove(leastBursTimetProcessIdx);     // remove it and run it
+                
+                if(currentlyRunning != null && currentlyRunning.startTime == -1)    // if there is a process has just started to run,
+                    currentlyRunning.startTime = time;                              // then set its start time to current time
+            }
+            
+            addToGanttChart(ganttChart, currentlyRunning);          // add currently running process at the current moment to Gantt Chart
+            time++;         // increment time
+        }
+        
+        return ganttChart;
+    }
+         
+    public void addToGanttChart(ArrayList<Integer> ganttChart, Process currentlyRunning){   // function to add a process's pid to Gantt Chart
+        if (currentlyRunning != null)               // if there is a process running at the time the function has called
+            ganttChart.add(currentlyRunning.pid);   // add this process's pid to Gantt Chart
+        else                                        
+            ganttChart.add(-1);                     // add -1, indicating no process was running at this time
+    }
+    
+    
+          public boolean allProcessesFinished(){  // function to check if all process has finished or not
+        for (Process p : processes) 
+            if(p.finishTime == -1)          // if any process has finish time = -1, then it hasn't finished yet
+                return false;
+        return true;
+    }
+            public void checkProcessesArrival(int time){    // check if any process has arrived at the given time
+        for (Process p : processes) 
+            if(p.arrivalTime == time)   // if a process arrived at the given time
+                readyQueue.add(p);      // then add it to the readyQueue
+    }
+                public static Process findLeastBurstTimeProcess(){   // function to find the process that has minimum burst time
+        int minBurstTime = Integer.MAX_VALUE;            // initilize minBurstTime to a very large number
+        Process leastBurstTimeProcess = null;
+        for (Process p : readyQueue) {
+            if(p.burstTime < minBurstTime){         // if process p has less burst time than leastBurstTimeProcess
+                leastBurstTimeProcess = p;          // make p the leastBurstTimeProcess           
+                minBurstTime = p.burstTime;
+            }
+        }
+        return leastBurstTimeProcess;       // return the leastBurstTimeProcess
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
